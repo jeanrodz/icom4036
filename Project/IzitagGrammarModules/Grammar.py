767c1d7922a956b81ce5522fd5e0e8a7e71a7d40
Jose Abreu <jose.abreu4@upr.edu>
2016-03-30 16:10:40 -0700

    Adjusted the iziTag grammar to read only assignments and the iziTitle(String text) function only

--- Project/IzitagGrammarModules/Grammar.py
+++ Project/IzitagGrammarModules/Grammar.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals, print_function
 from pypeg2 import *
 from Function import *
+from IzitagFunctions import *
+
 
 
 """
@@ -11,15 +13,18 @@ Module Description: Here we define how a function looks like in izitag
 """
 
 class Instruction(str):
-    grammar = [Assignment,Function]
+    grammar = [Assignment,IziTitle]
     
-class IziTag(List):
+class IziTag(str):
     grammar = "iziTag","{",attr("block",maybe_some(Instruction)),"}"
     
-string = "iziTag{ int foo := 6; zTitle( a, 9 , 0.8 , '');  }"
+string = "iziTag{ int foo := 6; iziTitle( 'jose');  }"
 
 f = parse(string, IziTag)
 
+print(f)
+"""
 print(len(f.block))
 print(f.block[0])
 print(f.block[1])
+"""
