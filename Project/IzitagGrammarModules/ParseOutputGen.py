82f1ab57bef9aa6620c2dfd31a2807526c8140e4
Jose Abreu <jose.abreu4@upr.edu>
2016-04-10 23:03:19 -0700

    DONT TOUCH !!

--- Project/IzitagGrammarModules/ParseOutputGen.py
+++ Project/IzitagGrammarModules/ParseOutputGen.py
@@ -10,25 +10,22 @@ Module Description: Here we will save the parser output in an output file
 #This will contain the source code of the user in 1 single string 
 outpath = "source.txt"
 parsestring = ''
-inpath = raw_input("Enter izitag source file path:  ")
+inpath = input("Enter izitag source file path:  ")
 
 with open(inpath, 'r') as content_file:
 	content = content_file.read()
-	#f is the parser
 	parsestring = content
 	content_file.close()
+parsestring.replace("str: ", "")
+print(parsestring)
 
 f = parse(parsestring, IziTag)
-
+print(f.block)
 with open(outpath, 'w+') as source_write:
-	source_write.write(content)
-	source_write.close() 
+	 for e in f.block: 
+	 	source_write.write(e +'\n')
+	 source_write.close()
+ 
 
 
-"""
-#Write Output to a text file to be interpreted for the intermidiate code 
-with open("out.txt", "wt") as out_file:
-    for e in f.block:
-        out_file.write(e +'\n')
- """
 
