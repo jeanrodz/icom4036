be9d02979c0b00cac92d9a7d9b875665a85026d0
Jean Rod <jean.rodriguez6@upr.edu>
2016-04-11 14:10:31 -0400

    almost there

--- Project/IzitagGrammarModules/Interpreter.py
+++ Project/IzitagGrammarModules/Interpreter.py
@@ -8,28 +8,50 @@ Module Description: Here we will use the dictionaries for the state machine :)
 /*****************************************************
 """
 
-stateDictionary = {(1, 'int'): 2,
-                   (1, 'iziPar'): 10, }
+token_stack = []
 
-def MainMachine(file,currentState,stateDictionary,maxStates):
-    
+def load_tokens(file): 
     with open(file,"rt") as in_file:
         for line in in_file:
             newline = line.replace("\n","")
-            print(newline)
             nl = newline.replace("[","")
             nl = nl.replace("]","")
             nl = nl.replace("'","")
             nl = nl.split(", ")
-            print(nl[0])
+            token_stack.append(nl)
             
-            
-    
-print(MainMachine("source.txt", 1, stateDictionary, 11))
+load_tokens('source.txt')            
+"""
+Data Definitions
+"""
+
+current_token = 0
+int_collection = {}
+float_collection = {}
+string_collection = {}
+array_collection = {}
+
+"""
+Parse-Map Dictionary
+"""
 
 
+def data_type_assignment(current_token):
+    if(token[0] == 'int'): int_collection[token[1]] = token[2]
+    elif (token[0] == 'float'): float_collection[token[1]] = token[2]
+    else : string_collection[token[1]] = token[2]
 
 
+parse_map = {
+    'int' : data_type_assignment,
+    'float' : data_type_assignment,
+    'string' : data_type_assignment,
+    }
 
+for token in token_stack:
+    parse_map[token[0]]
+    current_token = current_token + 1
 
+print(token_stack)
+print(string_collection)
 
