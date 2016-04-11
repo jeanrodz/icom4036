431c145a731f23429f76b60e969c1fba4eb89ada
Jean Rod <jean.rodriguez6@upr.edu>
2016-04-10 21:57:35 -0400

    fixes

--- Project/Files/reader.py
+++ Project/Files/reader.py
@@ -1,9 +1,11 @@
 
-outpath = "/source.txt"
-inpath = raw_input("Enter izitag source file path")
+outpath = "source.txt"
+inpath = raw_input("Enter izitag source file path:  ")
 
-with open(path, 'r') as content_file:
+with open(inpath, 'r') as content_file:
 	content = content_file.read()
+	content_file.close()
 
-with open(outpath, 'w') as source_write
-	source_write = content 
+with open(outpath, 'w+') as source_write:
+	source_write.write(content)
+	source_write.close() 
--- Project/IzitagGrammarModules/ParseOutputGen.py
+++ Project/IzitagGrammarModules/ParseOutputGen.py
@@ -8,14 +8,27 @@ Module Description: Here we will save the parser output in an output file
 """
 
 #This will contain the source code of the user in 1 single string 
-mySource = "iziTag{  String foo := 5;  iziPar(foo,'hola');  }"
+outpath = "source.txt"
+parsestring = ''
+inpath = raw_input("Enter izitag source file path:  ")
 
-#f is the parser
-f = parse(mySource, IziTag)
+with open(inpath, 'r') as content_file:
+	content = content_file.read()
+	#f is the parser
+	parsestring = content
+	content_file.close()
 
+f = parse(parsestring, IziTag)
+
+with open(outpath, 'w+') as source_write:
+	source_write.write(content)
+	source_write.close() 
+
+
+"""
 #Write Output to a text file to be interpreted for the intermidiate code 
 with open("out.txt", "wt") as out_file:
     for e in f.block:
         out_file.write(e +'\n')
-    
+ """
 
