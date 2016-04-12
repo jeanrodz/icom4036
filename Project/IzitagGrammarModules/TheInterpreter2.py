3ca49b99b22e4775a1fe45658daf06bb8c24814d
CRL-Nanito <tahiri.fuentes@upr.edu>
2016-04-12 10:02:16 -0400

    Wilson somos tus amigos! fixes to TheInterpreter2

--- Project/IzitagGrammarModules/TheInterpreter2.py
+++ Project/IzitagGrammarModules/TheInterpreter2.py
@@ -59,6 +59,10 @@ def section_function(condition):
 
 def paragraph_function(nl):
     print("<p>" + string_collection.get(nl[1]) + "</p>")
+    '''
+    with tag('p'):
+        text(string_collection.get(nl1))
+    '''
     
 #def list_function(nl):
  #   if (nl[2] == "ordered"):
@@ -68,31 +72,30 @@ def load_tokens(file):
     with open(file,"rt") as in_file:
         for line in in_file:
             newline = line.replace("\n","")
-            #if (newline == "iziSection") : newline = ['iziSection']
+            if (newline == "iziSection") : newline = "['iziSection', '']"
             nl = newline.replace("[","")
             nl = nl.replace("]","")
             nl = nl.replace("'","")
             nl = nl.split(", ")
             
-            #if ((nl[0] == 'int') & (nl[1] not in int_collection)): int_collection[nl[1]] = nl[2]
-            #elif ((nl[0] == 'string') & (nl[1] not in string_collection)): string_collection[nl[1]] = nl[2] 
-            #elif ((nl[0] == 'float') & (nl[1] not in float_collection)): float_collection[nl[1]] = nl[2]
-            #elif ((nl[0] == 'array') & (nl[1] not in array_collection)): array_collection[nl[1]] = nl[2]
-            #elif ((nl[0] == 'iziTitle') & (nl[1] in string_collection)): title_function(nl)
+            if ((nl[0] == 'int') & (nl[1] not in int_collection)): int_collection[nl[1]] = nl[2]
+            elif ((nl[0] == 'string') & (nl[1] not in string_collection)): string_collection[nl[1]] = nl[2] 
+            elif ((nl[0] == 'float') & (nl[1] not in float_collection)): float_collection[nl[1]] = nl[2]
+            elif ((nl[0] == 'array') & (nl[1] not in array_collection)): array_collection[nl[1]] = nl[2]
+            elif ((nl[0] == 'iziTitle') & (nl[1] in string_collection)): title_function(nl)
             #elif (nl[0] == 'iziSection'): section_function(condition)
-            #elif ((nl[0] == 'iziParagraph') & (nl[1] in string_collection)): paragraph_function(nl)
+            elif ((nl[0] == 'iziParagraph') & (nl[1] in string_collection)): paragraph_function(nl)
            # elif ((nl[0] == 'iziList') & (nl[1] in array_collection)): list_function(nl)      
             
             token_stack.append(nl)
 
-source_file ="source.txt"
-load_tokens(source_file)  
+load_tokens('source.txt')  
 
 for i in range(0, len(token_stack)):
     token = token_stack[i]
     if token[0] == 'iziSection':
         section = [ ]
-        for j in range(i, (len(token_stack))):
+        for j in range(i+1, (len(token_stack))):
             temp_token = token_stack[j]
             if(temp_token[0] != 'iziSection'):
                 section.append(temp_token)
@@ -107,47 +110,5 @@ print(string_collection)
 print(float_collection)
 print(array_collection)
 
-"""
-Data Definitions
-"""
-#current_token = token_stack[1]
 
 
-"""
-Parse-Map Dictionary
-"""
-"""    
-for index in range(0, len(token_stack)):
-    current_token = token_stack[index]
-
-    def data_type_assignment(current_token):
-        #print(current_token)
-        if(current_token[0] == 'int'): int_collection[current_token[1]] = current_token[2]
-        
-        elif (current_token[0] == 'float'): float_collection[current_token[1]] = current_token[2]
-        
-        elif ((current_token[0] == 'iziTitle') & (current_token[1] in string_collection)): title_function(current_token)
-        
-        else : string_collection[current_token[1]] = current_token[2]
-        
-    def title_function(current_token):
-        title_collection["title"] = string_collection.get(current_token[1])
-
-    parse_map = {
-        'int' : data_type_assignment(current_token),
-        'string' : data_type_assignment(current_token),
-        'float' : data_type_assignment(current_token),
-        'iziTitle' : data_type_assignment(current_token),
-        }
-
-#print (int_collection)
-#print (string_collection)
-#print (float_collection)
-#print (title_collection)
-"""
-"""
-print(token_stack)
-print(string_collection)
-print(int_collection)
-print(float_collection)
-"""
